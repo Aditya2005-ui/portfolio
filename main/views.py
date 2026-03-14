4ee2bacbb1c6899487e970a2a1aed5f3b19f2a56
Aditya Jagtap <adityaashokjagtap9699@gmail.com>
2026-03-14 20:44:46 +0530

    portfolio deployment

---
+++ main/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render, redirect
+from django.core.mail import send_mail
+from django.conf import settings
+
+def home(request):
+
+    if request.method == "POST":
+
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        message = request.POST.get("message")
+
+        subject = f"Portfolio message from {name}"
+
+        full_message = f"""
+Name: {name}
+Email: {email}
+
+Message:
+{message}
+"""
+
+        send_mail(
+            subject,
+            full_message,
+            settings.EMAIL_HOST_USER,
+            [settings.EMAIL_HOST_USER],
+        )
+
+        return redirect("/")   # prevents resubmit warning
+
+    return render(request, "home.html")
