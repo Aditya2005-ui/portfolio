eaf985305eb3209e40b693c9523dfb6baf3806a1
Aditya Jagtap <adityaashokjagtap9699@gmail.com>
2026-03-14 22:02:40 +0530

    added env variables for email

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -126,14 +126,12 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
+import os
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = 'adityaashokjagtap9356@gmail.com'
-EMAIL_HOST_PASSWORD = 'gyxl njcp fxyq lcog'
-
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
